22a940fb527e312cb16056ca7d409b50cdb482c1
yu1low <yu.chin827@gmail.com>
2016-01-06 14:41:00 +0000

    challenge4

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,11 +2,12 @@ class RankingController < ApplicationController
   
   def want
     want_count = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
-    @want_ranking = Item.find(:all,:order => "params[want_count[1]] DESC")
+    @want_ranking = Item.find(want_count.keys)
   end
   
   def have
-    @have_ranking = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    have_count = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    @have_ranking = Item.find(have_count.keys)
   end
   
 end
