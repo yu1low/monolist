9361f59e7153e815367fa0e6fc58bcbe900aa06d
yu1low <yu.chin827@gmail.com>
2016-01-07 15:56:22 +0000

    challenge4_3

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,13 +3,13 @@ class RankingController < ApplicationController
   def want
     want_count = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
     @item = Item.find(want_count.keys)
-    @want_ranking = @item.order('want_count.values desc')
-    
+    @want_ranking = @item.sort_by
   end
   
   def have
     have_count = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
-    @have_ranking = Item.find(have_count.keys)
+    @item = Item.find(have_count.keys)
+    @have_ranking = @item.sort_by
   end
   
 end
